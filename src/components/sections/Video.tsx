aef99ad7f1f9faf0530d96c33475251d932f6ab1
Junbeeom <wns_p@naver.com>
2024-07-14 01:40:29 +0900

    동영상 수정

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -14,7 +14,7 @@ function Video() {
         loop={true}
         poster="/assets/poster.jpg"
       >
-        <source src="/assets/main.mp4" type="video/mp4"></source>
+        <source src="/assets/main1.mp4" type="video/mp4"></source>
       </video>
     </Section>
   )
